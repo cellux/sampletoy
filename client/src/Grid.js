21f6befda6ad2b94a8a032b2cee308562bc3bde7
Ruzsa Balazs <balazs.ruzsa@disney.com>
2015-05-15 15:45:02 +0200

    event blocks limited to area of grid when dragged

--- client/src/Grid.js
+++ client/src/Grid.js
@@ -7,25 +7,55 @@ var Transform = require('famous/core/Transform');
 var View = require('famous/core/View');
 var Draggable = require('famous/modifiers/Draggable');
 
-function GridEvent(instr, pos) {
+function clamp(val, min, max) {
+  if (val < min) return min;
+  if (val > max) return max;
+  return val;
+}
+
+function GridEvent(parent, instr, pos) {
   events.EventEmitter.call(this);
   var self = this;
+  self.parent = parent;
   self.instr = instr;
   self.pos = pos;
+  self.size = [16,16];
   var modifier = new Modifier();
   modifier.alignFrom(function () {
-    return [self.pos, 0];
+    return self.pos;
   });
-  var draggable = new Draggable({
-    xRange: [-100,100],
+  var draggable = new Draggable();
+  draggable.on('start', function () {
+    var parentSize = self.parent.getSize();
+    if (parentSize) {
+      var pw = parentSize[0];
+      var ph = parentSize[1];
+      self.draggable.setOptions({
+        xRange: [-(self.pos[0] * pw), (1-self.pos[0]) * pw],
+        yRange: [-(self.pos[1] * ph), (1-self.pos[1]) * ph - self.size[1]],
+      });
+    }
+  });
+  draggable.on('end', function(data) {
+    var parentSize = self.parent.getSize();
+    if (parentSize) {
+      var pw = parentSize[0];
+      var ph = parentSize[1];
+      self.pos[0] += (data.position[0] / pw);
+      self.pos[1] += (data.position[1] / ph);
+      self.pos[0] = clamp(self.pos[0], 0, 1);
+      self.pos[1] = clamp(self.pos[1], 0, 1);
+      self.draggable.setPosition([0,0]);
+    }
   });
   var surface = new Surface({
-    size: [16,16],
+    size: self.size,
     properties: {
       backgroundColor: '#008',
     },
   });
   surface.pipe(draggable);
+  self.draggable = draggable;
   self.view = new View();
   self.view.add(modifier).add(draggable).add(surface);
 }
@@ -46,6 +76,7 @@ function Grid() {
       backgroundColor: '#ccc',
     },
   });
+  self.gridSurface = grid;
   self.view.add(grid);
   var line = new Surface({
     size: [1, undefined],
@@ -64,8 +95,8 @@ function Grid() {
 
 util.inherits(Grid, events.EventEmitter);
 
-Grid.prototype.add = function (instr, pos) {
-  pos = pos * (1.0 / this.resolution);
+Grid.prototype.add = function (instr, x, y) {
+  var pos = [x * (1.0 / this.resolution), y || 0];
   var event;
   if (this.freeEvents.length) {
     event = this.freeEvents.pop();
@@ -73,11 +104,15 @@ Grid.prototype.add = function (instr, pos) {
     event.pos = pos;
   }
   else {
-    event = new GridEvent(instr, pos);
+    event = new GridEvent(this, instr, pos);
     this.view.add(event.view);
   }
   this.events.push(event);
   return event;
 };
+
+Grid.prototype.getSize = function () {
+  return this.gridSurface.getSize();
+}
 
 module.exports = Grid;
